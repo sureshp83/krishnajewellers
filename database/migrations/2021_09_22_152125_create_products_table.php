a08840ee698e9216d4354f0e314508a9198e54fd
unknown <sureshp@hyperlinkinfosystem.net.in>
2021-09-27 09:49:07 +0530

    database change

--- database/migrations/2021_09_22_152125_create_products_table.php
+++ database/migrations/2021_09_22_152125_create_products_table.php
@@ -15,6 +15,7 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('product_id',100)->default(null)->nullable();
             $table->string('product_name')->default(null)->nullable();
             $table->bigInteger('category_id')->default(null)->nullable();
             $table->longText('description')->default(null)->nullable();
